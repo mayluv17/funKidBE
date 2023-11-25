4bdcba5a5a8de45f193c2e6497b1c6e490989fa5
“mayluv17” <mayowaibrahim009@gmail.com>
2023-11-25 02:06:52 +0100

    add: user and employee model to db

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -8,8 +9,13 @@ const errorHandler = require("./middleware/errorHandler");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
 const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
+const connectDb = require("./config/dbConn");
+
+//connect to mongo
+connectDb();
 // custom middleware logger
 app.use(logger);
 
@@ -55,4 +61,7 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("connected to mongoDb");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
