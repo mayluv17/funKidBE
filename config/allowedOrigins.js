d125caaa0624d6efde39d456930a88221889c45b
“mayluv17” <mayowaibrahim009@gmail.com>
2024-04-25 14:23:57 +0100

    add origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3600",
-  'http://localhost:5173'
+  'https://adventurequest.azurewebsites.net',
+  'http://127.0.0.1:5500',
+  'http://localhost:3500',
+  'http://localhost:3600',
+  'http://localhost:5173',
 ];
 
 module.exports = allowedOrigins;
